bc9cc94f48dddc781c5449e4b956175f6cd39252
Peter Johnstone <peter.wdj@gmail.com>
2019-07-21 22:08:25 +0100

    Do some firestore stuff

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 import './../models/user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ProfilePage extends StatelessWidget {
   final User user;
 
   ProfilePage({Key key, this.user}) : super(key: key);
+
+  void _doFirebaseThing() async {
+    var snapshots = await Firestore.instance.collection('users').getDocuments();
+    print('==========================');
+    print(snapshots.documents[0].data);
+    print('==========================');
+    print(snapshots.documents[0].documentID);
+    print('==========================');
+    print(snapshots.documents[0].metadata);
+    print('==========================');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +38,10 @@ class ProfilePage extends StatelessWidget {
                       image: NetworkImage(user.imageUrl()),
                     ),
                   ),
+                ),
+                RaisedButton(
+                  onPressed: _doFirebaseThing,
+                  child: Text('Do firebase thing'),
                 ),
               ],
             ),
