5db3546a363d2402aa6a5371ebeeb493be76c6c7
Peter Johnstone <peter.wdj@gmail.com>
2019-08-26 20:52:34 +0100

    Add some syntactic sugar to user model, add method to return stream of an instance, add method to upload a profile picture.

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,26 +1,41 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:uuid/uuid.dart';
+
+final Firestore firestore = Firestore.instance;
+final FirebaseStorage firebaseStorage = FirebaseStorage.instance;
+final StorageReference userImagesStorageReference = firebaseStorage.ref().child('userImages');
 
 class User {
   String id;
   String phoneNumber;
   String name;
   String bio;
+  String profileImageUrl = defaultImageUrl;
 
-  User(this.id, this.phoneNumber, this.name, this.bio);
+  User(this.id, this.phoneNumber, this.name, this.bio, this.profileImageUrl);
+
+  static const String defaultImageUrl = 'https://firebasestorage.googleapis.com/v0/b/deathtobars.appspot.com/o/userImages%2Fblank-profile-picture-973460_960_720.png?alt=media&token=064b0150-38e7-4755-8dac-516cfda4a9bf';
+
+  static const String defaultImageName = 'blank-profile-picture-973460_960_720.png';
 
   User.fromMap(Map<String, dynamic> map) {
     this.id = map['id'];
     this.phoneNumber = map['phoneNumber'];
     this.name = map['name'];
     this.bio = map['bio'];
+    this.profileImageUrl = map['profileImageUrl'] ?? profileImageUrl;
   }
 
   static Future<User> findOrCreateFromPhoneNumber(String phoneNumber) async {
-    QuerySnapshot snapshot = await Firestore.instance.collection('users').where('phoneNumber', isEqualTo: phoneNumber).getDocuments();
+    QuerySnapshot snapshot = await firestore.collection('users').where('phoneNumber', isEqualTo: phoneNumber).getDocuments();
 
     if (snapshot.documents.isEmpty) {
-      DocumentReference document = await Firestore.instance.collection('users').add({
-        'phoneNumber': phoneNumber
+      DocumentReference document = await firestore.collection('users').add({
+        'phoneNumber': phoneNumber,
+        'profileImageUrl': defaultImageUrl,
       });
 
       DocumentSnapshot snapshot = await document.get();
@@ -38,17 +53,43 @@ class User {
     return null;
   }
 
+  static Future<User> find(String id) async {
+    DocumentSnapshot ds = await firestore.collection('users').document(id).get();
+    return User.fromMap(ds.data);
+  }
+
   Future<void> update(Map<String, dynamic> attrs) async {
     this.id = attrs['id'] ?? this.id;
     this.name = attrs['name'] ?? this.name;
     this.phoneNumber = attrs['phoneNumber'] ?? this.phoneNumber;
     this.bio = attrs['bio'] ?? this.bio;
+    this.profileImageUrl = attrs['profileImageUrl'] ?? this.profileImageUrl;
 
-    Firestore.instance.collection('users').document(this.id).updateData(attrs);
+    firestore.collection('users').document(this.id).updateData(attrs);
   }
 
-  static Future<User> find(String id) async {
-    DocumentSnapshot ds = await Firestore.instance.collection('users').document(id).get();
-    return User.fromMap(ds.data);
+  Future<void> updateProfilePicture(File file) async {
+    // TODO: refactor / tidy up this method
+    StorageReference userImageRef = firebaseStorage.ref().child('userImages').child(Uuid().v1());
+    StorageUploadTask task = userImageRef.putFile(file);
+
+    StorageTaskSnapshot snapshot = await task.onComplete;
+
+    // TODO: work out how to delete the old profile picture here - delete method does not seem to be working
+
+    // do not delete the default profile picture
+    // StorageReference oldProfileImage = await firebaseStorage.getReferenceFromUrl(this.profileImageUrl);
+
+    // oldProfileImage.delete();
+    // if (await oldProfileImage.getName() != defaultImageName) {
+    // }
+
+    snapshot.ref.getDownloadURL().then((url) {
+      update({ 'profileImageUrl': url});
+    });
+  }
+
+  Stream<DocumentSnapshot> stream() {
+    return firestore.document('/users/${this.id}').snapshots();
   }
 }
