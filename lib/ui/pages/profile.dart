de50a63a0c5da97bd63c12adcd0ddd175019376e
Peter Johnstone <peter.wdj@gmail.com>
2019-08-26 20:53:03 +0100

    Add UI to upload profile picture; switch static build method for a streambuilder.

--- lib/ui/pages/profile.dart
+++ lib/ui/pages/profile.dart
@@ -1,47 +1,78 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
 
 import './../../models/user.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   final User user;
 
   ProfilePage({Key key, this.user}) : super(key: key);
 
-  void _doFirebaseThing() async { }
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  File file;
+  User user;
+
+  void _uploadPicture() async {
+    File file = await FilePicker.getFile(type: FileType.IMAGE);
+    user.updateProfilePicture(file);
+  }
 
   @override
+  initState() {
+    user = widget.user;
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Card(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: 120,
-                  height: 120,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: NetworkImage('https://www.some.ox.ac.uk/wp-content/uploads/2015/09/Peter.jpg'),
-                    ),
+    return StreamBuilder(
+      stream: user.stream(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          Map<String, dynamic> data = snapshot.data.data;
+          return Row(
+            children: <Widget>[
+              Expanded(
+                child: Card(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        width: 120,
+                        height: 120,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            image: NetworkImage(data['profileImageUrl']),
+                          ),
+                        ),
+                      ),
+                      Text('Name:'),
+                      Text(data['name']),
+                      Text('Bio:'),
+                      Text(data['bio']),
+                      RaisedButton(
+                        onPressed: _uploadPicture,
+                        child: Text('Upload profile picture'),
+                      ),
+                    ],
                   ),
                 ),
-                Text('Name:'),
-                Text(user.name),
-                Text('Bio:'),
-                Text(user.bio),
-                RaisedButton(
-                  onPressed: _doFirebaseThing,
-                  child: Text('Do firebase thing'),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
+              ),
+            ],
+          );
+        }
+
+        return Center(
+          child: Text('Loading...')
+        );
+      }
     );
   }
 }
