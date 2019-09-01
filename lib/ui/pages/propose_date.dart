38d83f8c0690cb2fbc6515736aa7d806b29cf8eb
Peter Johnstone <peter.wdj@gmail.com>
2019-09-01 20:43:05 +0100

    Add basic UI for proposing a date - currently does nothing.

--- lib/ui/pages/propose_date.dart
+++ lib/ui/pages/propose_date.dart
@@ -2,17 +2,49 @@ import 'package:flutter/material.dart';
 import './../../models/user.dart';
 import './../../models/date.dart';
 
-class ProposeDatePage extends StatelessWidget {
+class ProposeDatePage extends StatefulWidget {
   final User user;
 
   ProposeDatePage({Key key, this.user}) : super(key: key);
 
-  Widget build(BuildContext context) {
-    return Text('here is some text');
-  }
+  // Widget build(BuildContext context) {
+  //   return Text('here is some text');
+  // }
 
   // await Date.create({
   //   'proposerId': widget.user.id,
   //   'description': 'Try the mountain bike track in Brockwell park'
   // });
+
+  @override
+  _ProposeDatePageState createState() => _ProposeDatePageState();
+}
+
+class _ProposeDatePageState extends State<ProposeDatePage> {
+  final _dateSuggestionController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pinkAccent,
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(40),
+          child: Center(
+            child: Form(
+              key: _formKey,
+              child: TextFormField(
+                controller: _dateSuggestionController,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.beach_access),
+                  labelText: 'I want to...',
+                  helperText: 'This is it. Your time to shine. Go wild.',
+                )
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
