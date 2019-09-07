65278a0555a4e8be5cb574277809d6add877b08b
Peter Johnstone <peter.wdj@gmail.com>
2019-09-07 11:09:05 +0100

    Comment unused import for now

--- lib/ui/pages/propose_date.dart
+++ lib/ui/pages/propose_date.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import './../../models/user.dart';
-import './../../models/date.dart';
+//import './../../models/date.dart';
 
 class ProposeDatePage extends StatefulWidget {
   final User user;
